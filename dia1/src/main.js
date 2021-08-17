30f959d48fd70357eb9406a6880517d496f8c855
yamilemlf <yamilemlessa@gmail.com>
2021-08-17 14:54:48 -0300

    desafio dia 1 b academy

---
+++ dia1/src/main.js
@@ -0,0 +1,19 @@
+import './style.css'
+
+const app = document.querySelector('[data-js="app"]');
+
+app.innerHTML = `
+  <h1>B. Academy</h1>
+  <p>Boas vindas à semana de pré-work para o Bootcamp em React.js 😁</p>
+`
+const link = document.querySelector('[data-js="link"]');
+link.addEventListener('click', (event) => {
+  event.preventDefault();
+  if(app.hidden) {
+    app.hidden=false;
+  } else{
+    app.hidden=true;
+  }
+})
+
+
