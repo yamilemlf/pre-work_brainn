a9504f318099bcbc3fa071073f6b6d4fcc6e5908
yamilemlf <yamilemlessa@gmail.com>
2021-08-18 11:30:23 -0300

    Exercicio 2 dia 2: calculadora

--- dia2/src/main.js
+++ dia2/src/main.js
@@ -1,3 +1,6 @@
-import {immutable} from "./immutable"
+import { immutable } from "./immutable"
+import { calculadora, sum, sub, mult, div } from "./calculator"
 
 immutable();
+console.log('1 + 2 = ', calculadora(sum, 1, 2));
+
