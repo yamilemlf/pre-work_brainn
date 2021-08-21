4ccea0905d6de8aa269586b16cb45d47f554f845
yamilemlf <yamilemlessa@gmail.com>
2021-08-21 13:49:12 -0300

    Exercicio 2 do dia 3

--- dia3/src/form.js
+++ dia3/src/form.js
@@ -1,5 +1,6 @@
 const username = document.querySelector('[data-js="usernameInput"]')
 
+//Exercicio 1: Evento no input name:
 username.addEventListener('input', (e) => {
   e.target.value = primeiraLetraMaiuscula(e.target.value)
 })
@@ -8,6 +9,9 @@ const primeiraLetraMaiuscula = text => {
 	return text.toLowerCase()
 		.split(' ')
 		.map((word) => {
+      if(word.length === 0) {
+        return ''
+      }
       if(word !== "de"
       && word !== "da"
       && word !== "das"
@@ -21,4 +25,45 @@ const primeiraLetraMaiuscula = text => {
 		}).join(' ')
 }
 
+// Exercicio 2: Evento no select:
 
+//configurando e adicionando select ao formulario
+const form = document.querySelector('[data-js="form"]');
+const select = document.createElement('select');
+const colors = [
+  {value: '#0000ff', textContent: 'azul'},
+  {value: '#ffff00', textContent: 'amarelo'},
+  {value: '#ff0000', textContent: 'vermelho'},
+  {value: '#000000', textContent: 'preto'},
+  {value: '#00ff00', textContent: 'verde'}
+];
+
+colors.forEach(color => {
+  const option = document.createElement('option')
+  option.value = color.value;
+  option.textContent = color.textContent;
+  select.appendChild(option);
+});
+
+select.setAttribute('multiple', '');
+form. appendChild(select);
+
+//configurando os eventos select
+const colorsContainer = document.createElement('div');
+colorsContainer.style.display = 'flex'
+
+select.addEventListener('change', (e) => {
+  console.log(e.target.selectedOptions)})
+
+select.addEventListener('change', (e) => {
+  colorsContainer.innerHTML = '';
+  [... e.target.selectedOptions].map((option) => {
+    const div = document.createElement('div');
+    div.style.width = '100px';
+    div.style.height = '100px';
+    div.style.backgroundColor = option.value;
+    div.style.margin = '10px';
+    colorsContainer.appendChild(div);
+    document.body.appendChild(colorsContainer);
+  })
+});
